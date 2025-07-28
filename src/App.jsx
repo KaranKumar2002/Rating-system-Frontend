7d7e54e7e5d73d060ec7857a017fe43c8c725cfd
Karan Kumar <karan.kumar800433@gmail.com>
2025-07-28 15:39:34 +0530

    fixing the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './Pages/HomePage'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import Admin from './Pages/Admin'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import CardDetail from './Pages/CardDetail'
+import { useState } from 'react';
+import './App.css';
+import HomePage from './Pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Admin from './Pages/Admin';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import CardDetail from './Pages/CardDetail';
 
 function App() {
   const [userInput, setUserInput] = useState('');
+
   return (
     <>
       <NavBar userInput={userInput} setUserInput={setUserInput} />
@@ -21,12 +22,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/restaurants/:id" element={<CardDetail />} />
-          {/* Add more routes as needed */}
         </Routes>
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
